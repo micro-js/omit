e41a090250c55abf5f37a9dccfcc1e4f823c61cf
Andrew Shaffer <darawk@gmail.com>
2015-12-05 22:40:25 -0800

    use scoped package names; add travis.yml

--- test/index.js
+++ test/index.js
@@ -1,4 +1,19 @@
+/**
+ * Imports
+ */
 
-var assert = require('assert');
+var omit = require('..')
+var test = require('tape')
 
-describe('omit', function(){});
+/**
+ * Tests
+ */
+
+test('should work', function (t) {
+  var obj = {a: 1}
+  var newObj = omit(obj, 'a')
+
+  t.equal(newObj.a, undefined)
+  t.equal(Object.keys(newObj).length, 0)
+  t.end()
+})
